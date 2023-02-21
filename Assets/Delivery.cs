affc37c6795fbd3bef4c601108df13bc1bf35f4e
Denilson Laucsen da Rosa <denilsonlaucsen@gmail.com>
2023-02-21 16:37:54 -0300

    Add Destroy to Packages

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
+    [SerializeField] float destroyDelay = 0.5f;
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!! A big collision happened on 3rd Avenue");
@@ -11,14 +14,17 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked up");
+            hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
         }
         
-        if (other.tag == "Customer")
+        if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Delivered package!");
+            hasPackage = false;
         }
     }
 }
